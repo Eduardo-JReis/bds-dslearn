8426d9904735435b87ef17c5f7454f2a0a4962c6
Eduardo-JReis <edu.publicidade81@gmail.com>
2021-05-13 19:34:27 -0300

    Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe5brdebCPOb2uRwsSh8KwJYTTbNxXHG83QH1-uDxHaT3vFIW18fhjWuRKBFi6VvlZfD0&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9p3lEaamc_l43glp3QbtGe-As1m1mO8dwhA&usqp=CAU');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-14T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-14T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-14T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-14T03:00:00Z', 1);
