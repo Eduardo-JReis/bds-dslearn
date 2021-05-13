b7a227a9f18e7dc565f979842f2ab9bd9a792901
Eduardo-JReis <edu.publicidade81@gmail.com>
2021-05-12 23:09:06 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe5brdebCPOb2uRwsSh8KwJYTTbNxXHG83QH1-uDxHaT3vFIW18fhjWuRKBFi6VvlZfD0&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9p3lEaamc_l43glp3QbtGe-As1m1mO8dwhA&usqp=CAU');
