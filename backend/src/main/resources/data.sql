cf5ac622a66941c51d7f50024ebcc491d89cfb65
Eduardo-JReis <edu.publicidade81@gmail.com>
2021-05-19 23:09:06 -0300

    Section

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -21,3 +21,8 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe5brdebCPOb2uRwsSh8KwJYTTbNxXHG83QH1-uDxHaT3vFIW18fhjWuRKBFi6VvlZfD0&usqp=CAU', 1, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe5brdebCPOb2uRwsSh8KwJYTTbNxXHG83QH1-uDxHaT3vFIW18fhjWuRKBFi6VvlZfD0&usqp=CAU', 2, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives para a turma', 3, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe5brdebCPOb2uRwsSh8KwJYTTbNxXHG83QH1-uDxHaT3vFIW18fhjWuRKBFi6VvlZfD0&usqp=CAU', 0, 1);
+
+
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id ) VALUES ('Capitulo 1', 'Vamos começar', 1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe5brdebCPOb2uRwsSh8KwJYTTbNxXHG83QH1-uDxHaT3vFIW18fhjWuRKBFi6VvlZfD0&usqp=CAU', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id ) VALUES ('Capitulo 2', 'Vamos continuar', 2, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe5brdebCPOb2uRwsSh8KwJYTTbNxXHG83QH1-uDxHaT3vFIW18fhjWuRKBFi6VvlZfD0&usqp=CAU', 1, 1);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id ) VALUES ('Capitulo 3', 'Vamos finalizar', 3, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe5brdebCPOb2uRwsSh8KwJYTTbNxXHG83QH1-uDxHaT3vFIW18fhjWuRKBFi6VvlZfD0&usqp=CAU', 1, 2);
